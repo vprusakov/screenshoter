953b28bf796ac26c0303ab2d3afd174fec512c9a
Vyacheslav Prusakov <vyacheslav.prusakov@gmail.com>
2019-01-16 17:37:16 +0300

    Basic saving image functionality

--- src/main/java/ru/vprusakov/screenshoter/SaveImageAction.java
+++ src/main/java/ru/vprusakov/screenshoter/SaveImageAction.java
@@ -5,11 +5,33 @@ import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.project.Project;
 import org.jetbrains.annotations.NotNull;
 
+import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class SaveImageAction extends AnAction {
     public void actionPerformed(@NotNull AnActionEvent event) {
         BufferedImage image = new CodeImageBuilder(event).getSelectionScreenshot();
+        saveImage(image, event.getProject());
+    }
+
+    private void saveImage(BufferedImage codeImage, Project project) {
+        String fileDirPath = System.getProperty("user.home");
+        String fileSeparator = System.getProperty("file.separator");
+        String fileTimeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String fileName = "code-image_" + fileTimeStamp;
+        String fileFormat = "png";
+        String filePath = fileDirPath + fileSeparator + fileName + "." + fileFormat;
+
+        File file = new File(filePath);
+        try {
+            ImageIO.write(codeImage, fileFormat, file);
+        } catch (IOException e) {
+            System.out.println("An error occurred during writing.");
+        }
     }
 
 }
